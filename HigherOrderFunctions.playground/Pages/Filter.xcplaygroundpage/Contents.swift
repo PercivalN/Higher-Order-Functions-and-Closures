03671fb4db62ef59654f13c5a79bf2bf570e5c4a
PercivalN <percival.ngan@gmail.com>
2020-07-11 16:22:01 -0500

    Added higher order function template to file

--- HigherOrderFunctions.playground/Pages/Filter.xcplaygroundpage/Contents.swift
+++ HigherOrderFunctions.playground/Pages/Filter.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
-//: [Previous](@previous)
+// FILTER //
 
-import Foundation
 
-var str = "Hello, playground"
+// Filter numbers
+let scores: [Double] = [100.5, 95.5, 93.0, 90.5, 89.0, 80.0, 73.0, 65.5, 0.5]
 
-//: [Next](@next)
+
+
+// Filter strings
+let responses: [String] = ["Yes", "no", "yes", "no", "No", "YES", "yes", "yes", "NO"]
+
+
+
+
+// Challenge: use filter to divvy up the following array into two separate team lead group arrays. Each group should have an equal number of students. Jonah will take the first alphabetical half and Hayden will take the other.
+let students: [String] = ["Cora", "Eoin", "Glad", "Bryson", "Sam", "Ron"]
+
