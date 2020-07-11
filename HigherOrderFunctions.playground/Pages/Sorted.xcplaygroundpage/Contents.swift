1096f0e545e61cc8c3bc2fcbc8fad98b8b6f3ffc
PercivalN <percival.ngan@gmail.com>
2020-07-11 16:14:34 -0500

    Added Sorted and Map templates

--- HigherOrderFunctions.playground/Pages/Sorted.xcplaygroundpage/Contents.swift
+++ HigherOrderFunctions.playground/Pages/Sorted.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
-//: [Previous](@previous)
+// SORTED //
 
-import Foundation
+// Sort ints
+let numbers: [Int] = [1, 3, 5, 2, 4, 11, 6, 7, 9]
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+
+
+// Sort strings
+let names: [String] = ["Michael", "Shawn", "Hector", "Johnny", "Sam", "Percy"]
+
+
+
+
+// Challenge: Sort the following array into two separate arrays, one for ints and one for strings. Strings should be sorted alphabetically, and ints should be sorted from greatest to least.
+let randomArray: [Any] = ["Seven", 7, 6, "seven", 5, "Six", "0", 0]
+
+
+
